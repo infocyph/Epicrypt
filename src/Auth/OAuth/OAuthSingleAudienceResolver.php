f4937a99227de4771de6b5ce6b9d159bfc88690a
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-09 19:23:15 +0600

    refactor(oauth): bridge authorization audience resolver

--- src/Auth/OAuth/OAuthSingleAudienceResolver.php
+++ src/Auth/OAuth/OAuthSingleAudienceResolver.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Infocyph\Epicrypt\Auth\OAuth;
 
-final readonly class OAuthSingleAudienceResolver implements OAuthScopeAudienceResolverInterface
+final readonly class OAuthSingleAudienceResolver implements OAuthScopeAudienceResolverInterface, OAuthAuthorizationAudienceResolverInterface
 {
     public function resolve(OAuthClient $client, array $scopes): array
     {
