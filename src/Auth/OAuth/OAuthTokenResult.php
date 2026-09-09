547e25e09bd8d97c845141e831183c68a5e695b9
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-09 19:25:01 +0600

    feat(oauth): add token endpoint result

---
+++ src/Auth/OAuth/OAuthTokenResult.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Epicrypt\Auth\OAuth;
+
+final readonly class OAuthTokenResult
+{
+    private function __construct(
+        public ?OAuthTokenResponse $response,
+        public ?OAuthProtocolError $error,
+    ) {}
+
+    public static function success(OAuthTokenResponse $response): self
+    {
+        return new self($response, null);
+    }
+
+    public static function failure(OAuthErrorCode $error): self
+    {
+        return new self(null, new OAuthProtocolError($error));
+    }
+
+    public function successful(): bool
+    {
+        return $this->response !== null;
+    }
+}
